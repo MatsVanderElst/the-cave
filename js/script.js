fca096ad37c354ea8f779e0028209cc45f49941e
mats Van der Elst <matsvanderelst@matss-MacBook-Air.local>
2021-10-25 19:29:01 +0200

    refined styling and added scripts, realising to my HORROR that deadline was at 18h and not 24h

--- js/script.js
+++ js/script.js
@@ -1,24 +1,23 @@
 const buildScenario = () => {
   game = new Game(updateHTML);
-  scene = new GameScene("start", "you wake up in a misterious dark cave, 'How did i get here?' you ask yourself, When suddenly you hear a growl coming from the end of the cave...", "morning", "caveBackground");
+  scene = new GameScene("start", "you wake up in a misterious dark cave, 'How did i get here?' you ask yourself, When suddenly you hear a growl coming from the shadows...", "morning", "caveBackground");
   scene.addChoice("start game", "dragonScene", "runningAway");
-  scene.addAnimation("knight1", "expo", 3, 300);
   game.addScene(scene);
 
 
-  scene = new GameScene("dragonScene", "A giant fire breathing dragon appears in front of you! what do you do?", "swordSwing");
+  scene = new GameScene("dragonScene", "A giant fire breathing dragon appears in front of you! what do you do?", "dragonGrowl1", "caveBackground");
   scene.addChoice("fight", "fightScene", "swordSwing");
   scene.addChoice("run", "runningScene", "runningAway");
-  scene.addAnimation("knight1", "expo", 3, 400); 
+  scene.addAnimation("knight1", "expo", 3, 300); 
   scene.addAnimation("dragon1", "expo", 3, -300);
   game.addScene(scene);
   
   
-  scene = new GameScene("fightScene", "You're fighting choose what weapon you want to use, HURRY!!!");
-  scene.addChoice("Sword", "swordScene", "swordSwing");
-  scene.addChoice("Axe", "axeScene", "swordSwing");
-  scene.addAnimation("knight1", "expo", 3, 300); 
-  scene.addAnimation("dragon1", "expo", 3, -300);
+  scene = new GameScene("fightScene", "You're fighting choose what weapon you want to use, HURRY!!!","dragonGrowl1", "fightSceneBackground");
+  scene.addChoice("sword", "swordScene", "swordSwing");
+  scene.addChoice("magic", "axeScene", "swordSwing");
+  scene.addAnimation("sword1", "expo", 3, 300); 
+  scene.addAnimation("wand1", "expo", 3, -300);
   game.addScene(scene);
   
   
